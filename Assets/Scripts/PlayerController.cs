b863214d2898402ad074f2688391ff255ef6c58b
Arcis1420 <nmeckley55@proton.me>
2026-04-06 15:30:29 -0400

    ballgood

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,15 @@ using UnityEngine.InputSystem; //Don't miss this!
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private GameObject _ballPrefab;
-
-
-    private PlayerInput _input; //field to reference Player Input component
+    [SerializeField] GameObject _ballPrefab;
+    private PlayerInput _input;
     private Rigidbody2D _rigidbody;
+
+    //*NEW* remember facing direction (even after stopped)
+    private Vector2 _facingVector = Vector2.right;
+
+
+ 
 
     // Start is called before the first frame update
     void Start()
@@ -46,17 +50,22 @@ public class PlayerController : MonoBehaviour
                                 Quaternion.identity);
             //Get the Rigidbody 2D component from the new ball 
             //and set its velocity to x:-10f, y:0, z:0
-            ball.GetComponent<Rigidbody2D>().velocity = Vector2.up * 10f;
+            ball.GetComponent<BallController>()?.SetDirection(_facingVector);
         }
     }
 
     private void FixedUpdate()
     {
-        //set direction to the Move action's Vector2 value
         var dir = _input.actions["Move"].ReadValue<Vector2>();
-
-        //change the velocity to match the Move (every physics update)
         _rigidbody.velocity = dir * 5;
+
+        //to keep track of facing for aiming when stopped:
+        //Set _facingVector only while controls are moving
+        //(digital only gives 8 directions, analog is nicer)
+        if (dir.magnitude > 0.5)
+        {
+            _facingVector = _rigidbody.velocity;
+        }
     }
 }
 
